d3b513862b2b4c5af9cf510c8e562adb15dc9d2b
Pauross9 <paularosales92@gmail.com>
2022-02-22 14:14:56 -0300

    sincambios

--- src/main.js
+++ src/main.js
@@ -6,3 +6,11 @@ import data from './data/ghibli/ghibli.js';
 
 let resultado = filterData(data) 
 console.log(resultado)
+
+
+let filmVista = document.getElementById("btn-2");
+filmVista.addEventListener("click", filmVista);
+function filmVista(): {
+  document.getElementById("btn-1").style.display = "none";
+  document.getElementById("btn-3").style.display = "block";
+}
